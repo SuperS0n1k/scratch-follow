f74675305d16a28c9f231a92d1bcc418e8181f7c
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-06-02 18:04:38 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
 
+Scratch.INIT_DATA.LOGGED_IN_USER.model.username = "f_account"
+
 function followme() {
     $.ajax({
         type: "PUT",
-        url: "http://scratch.mit.edu/site-api/users/followers/" + "khuDDD" + "/add/",
+        url: "http://scratch.mit.edu/site-api/users/followers/" + "f_account" + "/add/",
         data: {usernames: Scratch.INIT_DATA.LOGGED_IN_USER.model.username}
     });
     window.open("https://scratch.mit.edu/users/khuDDD")
