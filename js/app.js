fc1c49ba8a380ab8c6cfa883b69c219ba7c9003c
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-06-02 18:09:33 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-
+import "https://cdn.scratch.mit.edu/scratchr2/static/__d227dda79ab53b07e3b30c100291aaac__//js/jquery.min.js"
 Scratch.INIT_DATA.LOGGED_IN_USER.model.username = "f_account"
 
 function followme() {
